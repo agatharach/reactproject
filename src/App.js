0f43d9ddb55e06dc8073658fe7f6a958ba4c5a95
agatharach <agatha@alterra.id>
2019-08-15 19:07:14 +0700

    home

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HeaderPostLogin from "./component/HeaderPostLogin";
 import HeaderPraLogin from "./component/HeaderPraLogin";
+import Home from "./page/Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/post" component={HeaderPostLogin} />
-        <Route exact path="/pra" component={HeaderPraLogin} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/post" component={HeaderPostLogin} />
+                <Route exact path="/pra" component={HeaderPraLogin} />
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
