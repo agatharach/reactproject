106b5f5a5cb6cc05a4361b58eb0174f14f81c0ec
agatharach <agatha@alterra.id>
2019-08-15 17:35:17 +0700

    samain

---
+++ src/page/HomePraLogin.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HeaderPraLogin from "../component/HeaderPraLogoin";
+import HeaderPostLogin from "../component/HeaderPostLogin";
+import { connect } from "unistore/react";
+import { actions } from "../store";
+
+function HomePraLogin() {
+    return (
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-12">
+                    <h1>Hello</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
