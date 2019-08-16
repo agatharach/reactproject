e443def68a6e3b83ee05e7f6bc94e47a966c40d6
yanuz93 <yanuzpurnomo@gmail.com>
2019-08-16 08:37:25 +0700

    sudah

--- src/page/coba.js
+++ src/page/coba.js
@@ -1,39 +1,81 @@
 import React from "react";
-// import NavBar from "../components/NavBar";
-// import SideBar from "../components/SideBar";
 import axios from "axios";
-// import Isi from "../components/Isi";
 
 const bookUrl = "https://www.goodreads.com/search/index.xml";
+const parser = new DOMParser();
 
 export default class Coba extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hasil: []
+      hasil: {
+        judul: [],
+        penulis: [],
+        tahun_terbit: [],
+        rating: [],
+        gambar: []
+      }
     };
   }
+
+  parsingXML = (doc, tags, posisi) => {
+    const xmlDoc = parser.parseFromString(doc, "text/xml");
+    const listSementara = xmlDoc.getElementsByTagName(tags);
+    const listHasil = [];
+    for (let index = 0; index < listSementara.length; index++) {
+      listHasil[index] = listSementara[index].childNodes[posisi].nodeValue;
+    }
+    return listHasil;
+  };
 
   componentDidMount() {
     const self = this;
     axios
       .get(bookUrl, {
-        headers: { "Access-Control-Allow-Origin": "*" },
         params: { key: "y1obKSI5hlBSWDILdLGMjg", q: "alchemist" }
       })
       .then(function(response) {
-        self.setState({ hasil: response.data });
+        self.setState({
+          hasil: {
+            judul: self.parsingXML(response.data, "title", 0),
+            penulis: self.parsingXML(response.data, "name", 0),
+            tahun_terbit: self.parsingXML(
+              response.data,
+              "original_publication_year",
+              0
+            ),
+            rating: self.parsingXML(response.data, "average_rating", 0),
+            gambar: self.parsingXML(response.data, "image_url", 0)
+          }
+        });
       })
       .catch(function(error) {
         console.log("ini error", error);
       });
   }
+  text = "";
+  fungsi = jumlah => {
+    for (let i = 0; i < jumlah; i++) {
+      this.text += this.state.hasil[i];
+    }
+    return this.text;
+  };
 
   render() {
     return (
       <div>
-        {console.log(this.state.hasil)}
         <p>Baru nyoba ini.</p>
+          {this.state.hasil.judul.map((value, index) => {
+            return (
+              <div>
+                <img src={this.state.hasil.gambar[index]} />
+                <h3>Judul : {value}</h3>
+                <h4>Penulis : {this.state.hasil.penulis[index]}</h4>
+                <h4>Tahun Terbit : {this.state.hasil.penulis[index]}</h4>
+                <h4>Rating : {this.state.hasil.tahun_terbit[index]}</h4>                
+              </div>
+            );
+          })}
       </div>
     );
   }
