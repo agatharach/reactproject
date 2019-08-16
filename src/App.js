3f694e7db42b690695d50c1501adf5d53b80972c
agatharach <agatha@alterra.id>
2019-08-16 08:28:13 +0700

    sign in dan profile

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "unistore/react";
+import { store } from "./store";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HeaderPostLogin from "./component/HeaderPostLogin";
 import HeaderPraLogin from "./component/HeaderPraLogin";
 import Home from "./page/Home";
+import SignIn from "./page/SignIn";
+import Profile from "./page/Profile";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/post" component={HeaderPostLogin} />
-                <Route exact path="/pra" component={HeaderPraLogin} />
-                <Route exact path="/" component={Home} />
-            </Switch>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/post" component={HeaderPostLogin} />
+                    <Route exact path="/pra" component={HeaderPraLogin} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={SignIn} />
+                    <Route exact path="/profile" component={Profile} />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,31 +1,24 @@
 import React from "react";
 import HeaderPraLogin from "../component/HeaderPraLogin";
 import HeaderPostLogin from "../component/HeaderPostLogin";
+import Quotes from "../component/Quotes";
 import { Link } from "react-router-dom";
-// import { connect } from "unistore/react";
-// import { actions } from "../store";
+import { connect } from "unistore/react";
+import { actions } from "../store";
 
-function Home() {
-    const is_login = true;
-    if (is_login) {
-        return (
-            <div className="container">
-                <HeaderPraLogin />
-                <div className="row d-flex align-items-center homeground">
-                    <div className="col-12 align-self-center text-center">
-                        <h1>Hello Guys</h1>
-                    </div>
-                </div>
-            </div>
-        );
-    } else {
+function Home(props) {
+    if (props.is_login) {
+        console.log(props.is_login);
         return (
             <div className="container">
                 <HeaderPostLogin />
                 <div className="row justify-content-center">
                     <div className="col-6 text-right">
                         <Link to="/movie">
-                            <img src="https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/6e/47/f6/6e47f680-ac54-21ff-a37a-3aab1a9970b0/DIS_AV_ENDGAME_FINAL_ENGLISH_US_WW_WW_ARTWORK_EN_2000x3000_1OWPBJ00000GQ6.lsr/268x0w.jpg" />
+                            <img
+                                src="https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/6e/47/f6/6e47f680-ac54-21ff-a37a-3aab1a9970b0/DIS_AV_ENDGAME_FINAL_ENGLISH_US_WW_WW_ARTWORK_EN_2000x3000_1OWPBJ00000GQ6.lsr/268x0w.jpg"
+                                alt="movie"
+                            />
                         </Link>
                     </div>
                     <div className="col-6 text-left">
@@ -34,13 +27,30 @@ function Home() {
                                 src="https://www.anime-planet.com/images/anime/covers/fullmetal-alchemist-437.jpg"
                                 width="268px"
                                 height="402px"
+                                alt="anime"
                             />
                         </Link>
                     </div>
                 </div>
             </div>
         );
+    } else {
+        console.log(props.is_login);
+        return (
+            <div className="container">
+                <HeaderPraLogin />
+                <div className="row d-flex align-items-center homeground">
+                    <div className="col-12 align-self-center text-center">
+                        <h1>Hello Guys</h1>
+                        <br />
+                        <Quotes />
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
-
-export default Home;
+export default connect(
+    "is_login",
+    actions
+)(Home);
