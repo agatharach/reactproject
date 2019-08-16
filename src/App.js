326680fb2a213b91e6bb2d996de6d3ba5d942a30
agatharach <agatha@alterra.id>
2019-08-16 11:03:21 +0700

    edit styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import "./App.css";
 import { Provider } from "unistore/react";
 import { store } from "./store";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import HeaderPostLogin from "./component/HeaderPostLogin";
-import HeaderPraLogin from "./component/HeaderPraLogin";
 import Home from "./page/Home";
 import SignIn from "./page/SignIn";
 import Profile from "./page/Profile";
 import Anime from "./page/Anime";
 import AnimeKategori from "./page/AnimeKategori";
-import Coba from './page/coba';
+import Coba from "./page/coba";
 
 function App() {
     return (
@@ -24,7 +22,6 @@ function App() {
                     <Route exact path="/anime" component={Anime} />
                     <Route path="/anime/:genre" component={AnimeKategori} />
                     <Route path="/books" component={Coba} />
-
                 </Switch>
             </BrowserRouter>
         </Provider>
--- src/page/Home.js
+++ src/page/Home.js
@@ -10,26 +10,28 @@ function Home(props) {
     if (props.is_login) {
         console.log(props.is_login);
         return (
-            <div className="container">
+            <div>
                 <HeaderPostLogin />
-                <div className="row justify-content-center">
-                    <div className="col-6 text-right">
-                        <Link to="/movie">
-                            <img
-                                src="https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/6e/47/f6/6e47f680-ac54-21ff-a37a-3aab1a9970b0/DIS_AV_ENDGAME_FINAL_ENGLISH_US_WW_WW_ARTWORK_EN_2000x3000_1OWPBJ00000GQ6.lsr/268x0w.jpg"
-                                alt="movie"
-                            />
-                        </Link>
-                    </div>
-                    <div className="col-6 text-left">
-                        <Link to="/anime">
-                            <img
-                                src="https://www.anime-planet.com/images/anime/covers/fullmetal-alchemist-437.jpg"
-                                width="268px"
-                                height="402px"
-                                alt="anime"
-                            />
-                        </Link>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-6 text-right">
+                            <Link to="/movie">
+                                <img
+                                    src="https://is3-ssl.mzstatic.com/image/thumb/Video113/v4/6e/47/f6/6e47f680-ac54-21ff-a37a-3aab1a9970b0/DIS_AV_ENDGAME_FINAL_ENGLISH_US_WW_WW_ARTWORK_EN_2000x3000_1OWPBJ00000GQ6.lsr/268x0w.jpg"
+                                    alt="movie"
+                                />
+                            </Link>
+                        </div>
+                        <div className="col-6 text-left">
+                            <Link to="/anime">
+                                <img
+                                    src="https://www.anime-planet.com/images/anime/covers/fullmetal-alchemist-437.jpg"
+                                    width="268px"
+                                    height="402px"
+                                    alt="anime"
+                                />
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,19 +39,20 @@ function Home(props) {
     } else {
         console.log(props.is_login);
         return (
-            <div className="container">
+            <div>
                 <HeaderPraLogin />
-                <div className="row d-flex align-items-center homeground">
-                    <div className="col-12 align-self-center text-center">
-                        <h1>Hello Guys</h1>
-                        <br />
-                        <Quotes />
+                <div className="container">
+                    <div className="row d-flex align-items-center homeground">
+                        <div className="col-12 align-self-center text-center">
+                            <h1>Hello Guys</h1>
+                            <br />
+                            <Quotes />
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
-
 }
 export default connect(
     "is_login",
